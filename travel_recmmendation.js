ef42783cc9b2fb05fbcd9569974271b2c272f06d
SRanuluge <sranuluge.dev@gmail.com>
2024-06-08 22:24:49 +1200

    feature:search logic handle

--- travel_recmmendation.js
+++ travel_recmmendation.js
@@ -1,19 +1,24 @@
-url = "./travel_recmmendation_api.json"
+url = "./travel_recmmendation_api.json";
 
-const searchKey = document.getElementById("searchbar-value")
-const serchBtn = document.getElementById("search-btn")
+const searchKey = document.getElementById("searchbar-value");
+const serchBtn = document.getElementById("search-btn");
 
-
-async function fetchData () {
+let res = {};
+async function fetchData() {
   const response = await fetch(url);
-  const reccomentaions = await response.json();
-  console.log(reccomentaions);
+  res = await response.json();
+  // console.log(res);
 }
 
-fetchData()
-serchBtn.addEventListener("click",()=>{
-    const searchKeyWord = searchKey.value.toLowerCase()
-    console.log(searchKeyWord);
-    
-})
-console.log(searchKey);
+fetchData();
+
+serchBtn.addEventListener("click", () => {
+  const searchKeyWord = searchKey.value.toLowerCase();
+
+  const key = Object.keys(res).filter((value) => {
+    const val = value.toLowerCase();
+    return val.indexOf(searchKeyWord) > -1;
+  });
+  const filteredSet = res[key[0]];
+  console.log(filteredSet);
+});
